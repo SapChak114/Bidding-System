0f28a255d2c479045338485160b613893f6230a7
Saptarshi Chakraborty <saptarshichakraborty@Saptarshis-MacBook-Pro.local>
2024-08-04 16:09:36 +0530

    Auction Management Service

---
+++ Auction Management System/src/main/java/com/biding/auction/AuctionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.biding.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionManagementSystemApplication.class, args);
+    }
+
+}
