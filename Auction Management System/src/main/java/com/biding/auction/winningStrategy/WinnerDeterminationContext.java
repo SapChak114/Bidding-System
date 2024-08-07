2e029d1b4fd9239fc0a9caa43a448c0c77b58ed2
Saptarshi Chakraborty <sap.saptarshi@gmail.com>
2024-08-07 16:48:36 +0530

    change in retry

--- Auction Management System/src/main/java/com/biding/auction/winningStrategy/WinnerDeterminationContext.java
+++ Auction Management System/src/main/java/com/biding/auction/winningStrategy/WinnerDeterminationContext.java
@@ -4,6 +4,7 @@ import com.biding.auction.dao.Bid;
 import com.biding.auction.enums.BiddingStrategy;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static com.biding.auction.constants.AuctionConstant.INVALID_BIDING_STRATEGY;
@@ -13,8 +14,10 @@ public class WinnerDeterminationContext {
     private final WinnerDeterminationStrategy strategy;
 
     public WinnerDeterminationContext(BiddingStrategy strategyKey) {
-        strategy = biddingStrategiesMap.computeIfAbsent(strategyKey, key -> {
-            throw new RuntimeException(INVALID_BIDING_STRATEGY);});
+        strategy = biddingStrategiesMap.get(strategyKey);
+        if (Objects.isNull(strategy)) {
+            throw new RuntimeException(INVALID_BIDING_STRATEGY);
+        }
     }
 
     public Optional<Bid> determineWinner(List<Bid> bids) {
